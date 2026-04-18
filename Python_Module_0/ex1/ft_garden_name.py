38c3f17c84e3ca603849a615acdc2ab27f2d22f7
Matias Vásquez <148326078+Matias171314@users.noreply.github.com>
2026-04-18 19:13:06 +0200

    fix

--- Python_Module_0/ex1/ft_garden_name.py
+++
@@ -1,4 +0,0 @@
-def ft_garden_name() -> None:
-    garden_name = input("Enter garden name: ")
-    print(f"Garden: {garden_name}")
-    print("Status: Growing well!")
--- Python_Module_0/ex6/ft_count_harvest_recursive.py
+++
@@ -1,9 +0,0 @@
-def ft_count_harvest_recursive() -> None:
-    def recursive_count(days: int) -> None:
-        if days == 0:
-            return
-        recursive_count(days - 1)
-        print(f"Day {days}")
-    days = int(input("Days until harvest: "))
-    recursive_count(days)
-    print("Harvest time!")
